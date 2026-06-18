7ec89fd0c96226dd921faaca45e458014ca8279d
asm-saim <saim777m@gmail.com>
2026-06-18 20:52:29 +0600

    card section perfectly alligned

--- js/index.js
+++ js/index.js
@@ -27,6 +27,12 @@ const displayWords = (words) => {
             <h1 class="font-bold text-2xl">${word.word}</h1>
             <p>Meaning /Pronunciation</p>
             <p class="font-medium text-lg">${word.meaning}/${word.pronunciation}</p>
+                        <div class="flex justify-between items-center">
+                <button class="btn bg-[#e8f4ff] hover:bg-[#1A91FF80]"> <i
+                        class="fa-solid fa-circle-info text-xl"></i></button>
+                <button class="btn bg-[#e8f4ff] hover:bg-[#1A91FF80]"><i
+                        class="fa-solid fa-volume-high text-xl"></i></button>
+            </div>
         </div>`
         // 4
         showWords.append(finalCard)
